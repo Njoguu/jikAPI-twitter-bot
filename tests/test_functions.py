c8ef33d10595ac136af7ba95076c88acbe12a747
Njoguu <njoguuallan@gmail.com>
2023-01-28 22:50:56 +0300

    Fix: Failing Tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,18 +1,22 @@
 import pytest
-from src import jikapibot
+import sys
+import os
+path = os.getcwd()
+sys.path.append(path+"/src/")
+from jikapibot import like_mentions, like_tweets, retweet_mentions
 
 
 # check that like_tweets function is callable
 def test_like_tweet_is_callable():
-    assert callable(jikapibot.like_tweets)
+    assert callable(like_tweets)
 
 # check that like_mentions function is callable
 def test_like_mention_is_callable():
-    assert callable(jikapibot.like_mentions)
+    assert callable(like_mentions)
 
 # check that retweet_mentions function is callable
 def test_retweet_tweet_is_callable():
-    assert callable(jikapibot.retweet_mentions)
+    assert callable(retweet_mentions)
     
 
 if __name__ == '__main__':
